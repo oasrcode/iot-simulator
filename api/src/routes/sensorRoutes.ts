df0fedfb377fc747274ae1c01d93d2fc9354ef40
oasrcode <oasrcode@gmail.com>
2024-06-21 23:06:06 +0000

    improved logic for get sensordata by serialnumber

--- api/src/routes/sensorRoutes.ts
+++ api/src/routes/sensorRoutes.ts
@@ -1,30 +1,42 @@
-import express, { Router,Request,Response } from "express";
+import express, { Router, Request, Response } from "express";
 import SensorController from "../controller/sensorController";
-
 
 const sensorRouter: Router = express.Router();
 
 const sensorController = new SensorController();
 
-sensorRouter.get('/airSensors/:serialnumber', async (req: Request, res: Response) => {
-    const {serialnumber} = req.params
+sensorRouter.get(
+  "/airSensors/:serialnumber",
+  async (req: Request, res: Response) => {
+    await getSensorData("air", req, res);
+  }
+);
 
-    if(!serialnumber){
-        res.status(400).send(`El valor de serialnumber no es válido.`)
-    }
-    const sensorData = await sensorController.getAirSensorMongoData(serialnumber); 
+sensorRouter.get(
+  "/waterSensors/:serialnumber",
+  async (req: Request, res: Response) => {
+    await getSensorData("water", req, res);
+  }
+);
 
-    res.status(200).send(sensorData)
-});
+async function getSensorData(type: string, req: Request, res: Response) {
+  const { serialnumber } = req.params;
 
-sensorRouter.get('/waterSensors/:serialnumber', async (req: Request, res: Response) => {
-    const {serialnumber} = req.params
+  if (!serialnumber) {
+    res.status(400).send(`El valor de serialnumber no es válido.`);
+  }
 
-    if(!serialnumber){
-        res.status(400).send(`El valor de serialnumber no es válido.`)
-    }
-    const sensorData = await sensorController.getWaterSensorMongoData(serialnumber); 
+  try {
+    const sensorData =
+      type == "air"
+        ? await sensorController.getAirSensorMongoData(serialnumber)
+        : await sensorController.getWaterSensorMongoData(serialnumber);
 
-    res.status(200).send(sensorData)
-});
+    return res.status(200).send(sensorData);
+  } catch (error) {
+    console.log("Error al traerse los datos del sensor: ", error);
+    res.status(500).send("Error al traerse los datos del sensor");
+  }
+}
+
 export default sensorRouter;
